5717bb32b94b8fe77bcfeaf47179993015cc0806
Shreyas <shrey4s@live.com>
2020-05-27 12:34:04 +0530

    Hotel settings

--- resources/views/Admin/Settings/general-settings.php
+++ resources/views/Admin/Settings/general-settings.php
@@ -1,3 +1,33 @@
 <div class="settings-container">
-<h2 class="font-weight-light">General settings</h2>
+        <h2 class="font-weight-light">General settings</h2>
+        <div class="settings-section">
+                <h4 class="font-weight-light">Hotel Info</h4>
+                <p>This info is displayed on the Hotel page.</p>
+                <div class="row mt-3">
+                        <div class="col-sm-4">
+                                <div class="input-group">
+                                        <div class="input-group-prepend">
+                                                <span class="input-group-text">$</span>
+                                        </div>
+                                        <input type="text" name="Hotel/name_of_the_hotel" id="Hotel/name_of_the_hotel" class="form-control" placeholder="Hotel Name" value="<?php echo $plugin->options->get( 'Hotel/name_of_the_hotel' ); ?>" />
+                                </div> 
+                        </div>
+                        <div class="col-sm-4">
+                               <div class="input-group">
+                                        <div class="input-group-prepend">
+                                                <span class="input-group-text">$</span>
+                                        </div>
+                                        <input type="text" name="Hotel/address_line_1" id="Hotel/address_line_1" class="form-control" placeholder="Address 1" value="<?php echo $plugin->options->get( 'Hotel/address_line_1' ); ?>" />
+                                </div>                                 
+                        </div>
+                        <div class="col-sm-4">
+                                <div class="input-group">
+                                        <div class="input-group-prepend">
+                                                <span class="input-group-text">$</span>
+                                        </div>
+                                        <input type="text" name="Hotel/address_line_2" id="Hotel/address_line_2" class="form-control" placeholder="Address 2" value="<?php echo $plugin->options->get( 'Hotel/address_line_2' ); ?>" />
+                                </div>
+                        </div>
+                </div>
+        </div>
 </div>
--- resources/views/Admin/settings.php
+++ resources/views/Admin/settings.php
@@ -9,8 +9,8 @@
                                <div class="row">
                                        <div class="col-sm-2">
                                                <div class="bshb-nav nav flex-column nav-pills mt-5" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                                        <a class="nav-link active" id="general-settings-tab" data-toggle="pill" href="#general-settings" role="tab" aria-controls="general-settings" aria-selected="true">Home</a>
-                                                        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+                                                        <a class="nav-link active" id="general-settings-tab" data-toggle="pill" href="#general-settings" role="tab" aria-controls="general-settings" aria-selected="true">General</a>
+                                                        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Room</a>
                                                         <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
                                                         <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
                                                 </div>

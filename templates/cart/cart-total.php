d329fd9c3983c238785c65203408b10c2842db00
Shreyas <shrey4s@live.com>
2020-09-11 05:31:23 +0530

    cart taxes and total - function implement

--- templates/cart/cart-total.php
+++ templates/cart/cart-total.php
@@ -1,27 +1,25 @@
-<?php var_dump($data); ?>
 <div class="row">
         <div class="col-sm-12">
         <div class="cart-sub-total">
-                        <h3><?php echo esc_html("Sub Total" , "bshb"); ?></h3>
-                        <h1><?php echo bshb_format_currency($data['sub_total']);?></h1>
-                </div>   
-                
+                        <h2><?php echo esc_html("Sub Total" , "bshb"); ?></h2>
+                        <h2><?php echo bshb_format_currency($data['sub_total']);?></h2>
+                </div>                
                        
                         <?php foreach($data['tax'] as $tax) { ?>
                                         <div class="cart-tax">
-                                                <div class="tax-rate">
-                                                        <h5><?php echo $tax['tax_name'] ?></h5>
-                                                        <h6><?php echo '   ' . $tax['tax_rate'] ?></h6>
+                                                <div class="tax-rate ">
+                                                        <span><?php echo $tax['tax_name'] ?></span>
+                                                        <span><?php echo $tax['tax_rate']; ?></span>
                                                 </div>                                
                                                  <div class="tax-amount">
-                                                        <h4><?php echo bshb_format_currency($tax['tax']); ?></h4>
+                                                        <h2><?php echo bshb_format_currency($tax['tax']); ?></h2>
                                                 </div>
                                         </div>            
                         <?php } ?>
                 
                 <div class="cart-total">
-                        <h3><?php echo esc_html("Total" , "bshb"); ?></h3>
-                        <h1><?php echo bshb_format_currency($data['total']);?></h1>
+                        <h1 class="total "><?php echo esc_html("Total" , "bshb"); ?></h1>
+                        <h1 class="total-amount"><?php echo bshb_format_currency($data['total']);?></h1>
                 </div>        
         </div>
 </div>

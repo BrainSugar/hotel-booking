aab91bc500f05731967c96958723c07e554285cf
rkariath <rkariath@gmail.com>
2020-10-18 11:38:15 +0530

    OOP Implementation

--- plugin/Admin/Columns/RoomTypeColumns.php
+++ plugin/Admin/Columns/RoomTypeColumns.php
@@ -1,61 +1,54 @@
 <?php
 namespace Brainsugar\Admin\Columns;
 
-use Brainsugar\CustomPostTypes\RoomTypeCustomPostType;
-use Brainsugar\Model\Room;
+if ( !class_exists( 'RoomTypeColumns' ) ) {
+    class RoomTypeColumns extends ColumnManager implements RenderColumnsInterface {
+        public function list_columns( $existing_columns ) {
+            if ( empty( $existing_columns ) && ! is_array( $existing_columns ) ) {
+                $existing_columns = array();
+            }
 
-class RoomTypeColumns extends ManageColumns {
-    protected $postType;
-    
-    public function __construct() {
-        $postType = RoomTypeCustomPostType::getPostType();
+            unset( $existing_columns['comments'], $existing_columns['date'] );
 
-        $this->postType = $postType;
-		$this->bshb_register_actions_and_filters();
-    }
+            $columns                    = array();
+            $columns['max_guests']      = esc_html__( 'Max Guests', 'bshb-td' );
+            $columns['rack_rate']       = esc_html__( 'Rack Rate', 'bshb-td' );
+            $columns['rooms_available'] = esc_html__( 'Rooms Available', 'bshb-td' );
+            $columns['image']           = esc_html__( 'Image', 'bshb-td' );
 
-    public function bshb_register_actions_and_filters() {            
-        add_filter( "manage_{$this->postType}_posts_columns", array( $this, "bshb_list_metabox_columns" ) );
-		add_filter( "manage_{$this->postType}_posts_custom_column", array( $this, "bshb_render_metabox_columns" ) );
-    }
-
-    public function bshb_list_metabox_columns( $existing_columns ) {
-		if ( empty( $existing_columns ) && ! is_array( $existing_columns ) ) {
-			$existing_columns = array();
+            return array_merge( $existing_columns, $columns );
         }
 
-        unset( $existing_columns['comments'], $existing_columns['date'] );
+        public function render_columns( $column ) {
+            global $post;
 
-		$columns                      = array();
-		$columns['max_guests']        = esc_html__( 'Max Guests', 'bshb-td' );
-		$columns['rack_rate']         = esc_html__( 'Rack Rate', 'bshb-td' );
-        $columns['rooms_available']   = esc_html__( 'Rooms Available', 'bshb-td' );
-        
-		return array_merge( $existing_columns, $columns );
-	}
+            switch( $column ){
+                case 'max_guests':
+                    $max_guests     = get_post_meta( $post->ID, "_{$this->prefix}_headcount", true );
+                    $max_adults     = $max_guests['max_adults'] . 'A';
+                    $max_children   = $max_guests['max_children'] . 'C';
+                    $max_guests     = "$max_adults, $max_children";
 
-	public function bshb_render_metabox_columns( $column ) {
-		global $post;
+                    echo $max_guests ;
+                break;
 
-        switch( $column ){
-            case 'max_guests':
-                $max_guests     = get_post_meta( $post->ID, '_bshb_headcount', true );
-                $max_adults     = $max_guests['max_adults'] . 'A';
-                $max_children   = $max_guests['max_children'] . 'C';
-                $max_guests     = "$max_adults, $max_children";
+                case 'rack_rate':
+                    $rack_rate = get_post_meta( $post->ID, "_{$this->prefix}_rack_rate", true );
+                    echo $rack_rate ;
+                break;
 
-                echo $max_guests ;
-            break;
+                case 'rooms_available':
+                    $stock_rooms = get_post_meta( $post->ID, "_{$this->prefix}_stock_rooms", true );
+                    echo $stock_rooms ;
+                break;
 
-            case 'rack_rate':
-                $rack_rate = get_post_meta( $post->ID, '_bshb_rack_rate', true );
-                echo $rack_rate ;
-            break;
-
-            case 'rooms_available':
-                $stock_rooms = get_post_meta( $post->ID, '_bshb_stock_rooms', true );
-                echo $stock_rooms ;
-            break;
+                case 'image':
+                    if ( has_post_thumbnail() ) {
+                        $featured_image = the_post_thumbnail( array(40, 40) );
+                        echo $featured_image;
+                    }
+                break;
+            }
         }
-	}
+    }
 }

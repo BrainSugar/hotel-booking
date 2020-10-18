82de978664dfedc94a9c0821bd56af5df57f82b2
rkariath <rkariath@gmail.com>
2020-10-18 11:56:09 +0530

    OOP Implementation

--- plugin/Admin/Metaboxes/ServiceMetabox.php
+++ plugin/Admin/Metaboxes/ServiceMetabox.php
@@ -1,153 +1,137 @@
 <?php
 namespace Brainsugar\Admin\Metaboxes;
 
-use Brainsugar\CustomPostTypes\ServiceCustomPostType;
-use Brainsugar\Admin\Collections\CollectionsList;
+use Brainsugar\Admin\Collections\CollectionsRepository;
 
-class ServiceMetabox extends ManageMetaboxes {
-    protected $post_type;
-    protected $prefix;
-    
-    public function __construct() {
-        $post_type = ServiceCustomPostType::getPostType();
+if ( !class_exists( 'ServiceMetabox' ) ) {
+    class ServiceMetabox extends MetaboxManager implements RenderMetaboxInterface {
+        public function register_fields() {
+            $service_metabox = new_cmb2_box( array(
+                'id'            => "_{$this->prefix}_service_metabox",
+                'title'         => __( 'Service Details', 'bshb-td' ),
+                'object_types'  => array( $this->post_type ),
+                'classes'       => array( $this->prefix ),
+                'cmb_styles'    => false,
+            )   );
 
-        $this->post_type = $post_type;
-        $this->prefix = 'bshb';
+            $service_metabox->add_field( array(
+                'id'            => "_{$this->prefix}_heading",
+                'type'          => 'text',
+                'render_row_cb' => array( $this, "render_heading" ),
+            )   );
 
-		$this->bshb_register_hooks();
-    }
-    
-    private function bshb_register_hooks() {
-        add_action( 'cmb2_init', array( $this, "{$this->prefix}_register_fields" ) );
-        add_action( 'admin_head-post.php', array( $this, "{$this->prefix}_hide_publishing_actions" ) );
-        add_action( 'admin_head-post-new.php', array( $this, "{$this->prefix}_hide_publishing_actions" ) );
-    }
+            $service_metabox->add_field( array(
+                'id'            => "_{$this->prefix}_service_operation",
+                'name'          => __( 'Operation', 'bshb-td' ),
+                'type'          => 'select',
+                'options_cb'    => function(){ return CollectionsRepository::get_service_operations(); },
+                'attributes'    => array(
+                    'class'         => 'form-control',
+                    'required'      => 'required',
+                ),
+                'col_size'      => '8',
+                'icon'          => 'fas fa-rupee-sign',
+                'tip'           => __( 'How does this Service affect the Total Amount payable', 'bshb-td' ),
+                'show_names'    => false,
+                'before'        => array( $this, "before_field" ),
+                'after'         => array( $this, "after_field" ),
+            )   );
 
-    public function bshb_register_fields() {
-        $service_metabox = new_cmb2_box( array(
-            'id'                => "_{$this->prefix}_service_metabox",
-            'title'             => __( 'Service Details', 'bshb-td' ),
-            'object_types'      => array( $this->post_type ),
-            'classes'           => array( $this->prefix ),
-            'cmb_styles'        => false,
-        )   );
+            $service_metabox->add_field( array(
+                'id'            => "_{$this->prefix}_service_amount",
+                'name'          => __( 'Amount', 'bshb-td' ),
+                'type'          => 'text',
+                'attributes'    => array(
+                    'type'          => 'number',
+                    'min'           => '1',
+                    'max'           => '999999',
+                    'class'         => 'form-control',
+                    'placeholder'   => 'Amount',
+                    'required'      => 'required',
+                ),
+                'col_size'      => '8',
+                'icon'          => 'fas fa-rupee-sign',
+                'tip'           => __( 'How much does this Service cost?', 'bshb-td' ),
+                'show_names'    => false,
+                'before'        => array( $this, "before_field" ),
+                'after'         => array( $this, "after_field" ),
+            )   );
 
-        $service_metabox->add_field( array(
-            'id'                => "_{$this->prefix}_heading",
-            'type'              => 'text',
-            'render_row_cb'     => array( $this, "{$this->prefix}_render_heading" ),
-        )   );
+            $service_metabox->add_field( array(
+                'id'            => "_{$this->prefix}_service_availability",
+                'name'          => __( 'Service Availability', 'bshb-td' ),
+                'type'          => 'select',
+                'attributes'    => array(
+                    'class'         => 'form-control',
+                ),
+                'col_size'      => '8',
+                'options_cb'    => function(){ return CollectionsRepository::get_service_availability(); },
+                'icon'          => 'fas fa-rupee-sign',
+                'tip'           => __( 'Is the Service available right now?', 'bshb-td' ),
+                'show_names'    => false,
+                'before'        => array( $this, "before_field" ),
+                'after'         => array( $this, "after_field" ),
+            )   );
 
-        $service_metabox->add_field( array(
-            'id'                => "_{$this->prefix}_service_operation",
-            'name'              => __( 'Operation', 'bshb-td' ),
-            'type'              => 'select',
-            'options_cb'        => array( $this, "{$this->prefix}_get_service_operations" ) ,
-            'show_option_none'  => false,
-            'attributes'        => array(
-                'class'             => 'form-control',
-                'required'          => 'required',
-            ),
-            // 'column'            => array(
-            //     'position'          => 2,
-            //     'name'              => 'Operation',
-            // ),
-            'show_names'        => false,
-            'before'            => function(){
-                echo 
-                '<div class="col-8">';
-            },
-            'after'             => function(){
-                echo 
-                '<p class="option-desc">Choose how this Service affects the total amount of a Reservation</p>
-                </div>';
-            },
-        )   );
-
-        $service_metabox->add_field( array(
-            'id'                => "_{$this->prefix}_service_amount",
-            'name'              => __( 'Amount', 'bshb-td' ),
-            'type'              => 'text',
-            'attributes'        => array(
-                'type'              => 'number',
-                'min'               => '1',
-                'max'               => '999999',
-                'class'             => 'form-control',
-                'placeholder'       => 'Amount',
-                'required'          => 'required',
-            ),
-            'show_names'        => false,
-            'before'            => function(){
-                echo 
-                '<div class="col-8">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">
-                                <i class="fas fa-rupee-sign"></i>
-                            </span>
-                        </div>';
-            },
-            'after'             => function(){
-                echo 
-                    '</div>
-                    <p class="option-desc">How much does this Service cost</p>
-                </div>';
-            },
-        )   );
-
-        $service_metabox->add_field( array(
-            'id'                => "_{$this->prefix}_service_description",
-            'name'              => __( 'Service Description', 'bshb-td' ),
-            'type'              => 'textarea_small',
-            'attributes'        => array(
-                'class'             => 'form-control',
-                'placeholder'       => 'Description',
-                'rows'              => 2,
-            ),
-            'show_names'        => false,
-            'before'            => function(){
-                echo 
-                '<div class="col-8">';
-            },
-            'after'             => function(){
-                echo 
-                '<p class="option-desc">Optional Description for this Service</p>
-                </div>';
-            },
-        )   );
-    }
-    
-    public function bshb_render_heading() {
-        ?>
-            <div class="col-12 my-4">
-                <div class="heading_field">
-                    <h4 class="metabox-main-heading font-weight-light">
-                        <?php esc_html_e( 'Service Details', 'bshb-td' ) ?>
-                    </h4>
-
-                    <p class="metabox-sub-heading setting-desc">
-                        <?php esc_html_e( 'Additional Services provided for Guests', 'bshb-td' ) ?>
-                    </p>
-                </div>
-            </div>
-        <?php 
-    }
-
-    public function bshb_hide_publishing_actions() {
-        global $post;
-        
-        if( $post->post_type == ServiceCustomPostType::getPostType() ) {
-            echo 
-            '<style type="text/css">
-                #misc-publishing-actions,
-                #minor-publishing-actions {
-                    display:none;
-                }
-            </style>';
+            $service_metabox->add_field( array(
+                'id'            => "_{$this->prefix}_service_description",
+                'name'          => __( 'Service Description', 'bshb-td' ),
+                'type'          => 'textarea_small',
+                'attributes'    => array(
+                    'class'         => 'form-control',
+                    'placeholder'   => 'Description',
+                    'rows'          => '3',
+                ),
+                'col_size'      => '12',
+                'icon'          => 'fas fa-rupee-sign',
+                'tip'           => __( 'Optional Description for this Service', 'bshb-td' ),
+                'show_names'    => false,
+                'before'        => array( $this, "before_field" ),
+                'after'         => array( $this, "after_field" ),
+            )   );
         }
-    }
-    
-    public function bshb_get_service_operations() {
-        return CollectionsList::bshb_get_service_operations() ;
+        
+        public function render_heading() {
+            ?>
+                <div class="col-12 my-4">
+                    <div class="heading_field">
+                        <h4 class="metabox-main-heading font-weight-light">
+                            <?php esc_html_e( 'Service Details', 'bshb-td' ) ?>
+                        </h4>
+
+                        <p class="metabox-sub-heading setting-desc">
+                            <?php esc_html_e( 'Additional Services provided for Guests', 'bshb-td' ) ?>
+                        </p>
+                    </div>
+                </div>
+            <?php 
+        }
+
+        public function before_field( $field_args, $field ) {
+            ?>
+                <div class="col-<?= $field->args( 'col_size' ); ?>">
+                    <?php if (! empty( $field->args( 'icon' ) ) ) : ?>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="<?= $field->args( 'icon' ); ?>"></i>
+                                </span>
+                            </div>
+                    <?php endif; ?>
+            <?php
+        }
+
+        public function after_field( $field_args, $field ) {
+            ?>
+                    <?php if (! empty( $field->args( 'icon' ) ) ) : ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if (! empty( $field->args( 'tip' ) ) ) : ?>
+                        <p class="option-desc"><?= $field->args( 'tip' ); ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php
+        }
     }
 }
